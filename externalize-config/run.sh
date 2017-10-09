8f4fc4e651e73305c8321768913be470d0aed6f0
Mario Guerrero <guedim@gmail.com>
2017-10-09 00:57:35 -0500

    delete target files

--- externalize-config/run.sh
+++ externalize-config/run.sh
@@ -2,4 +2,4 @@
 
 docker run -d -p 80:8080 -e PROFILE=production -e GIT_REPO="https://github.com/guedim/spring-projects" \
     -e LABEL=master -e REL_PATH="externalize-config/src/main/resources" \
-    -e APP_NAME="application" guedim/externalize-config
+    -e APP_NAME="application" guedim/spring-externalize-config
