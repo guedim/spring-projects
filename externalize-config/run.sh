f7cb9536d9c545262c191b6e1cc745f26feb4eba
Mario Guerrero <guedim@gmail.com>
2017-10-09 00:39:59 -0500

    Create run.sh

---
+++ externalize-config/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+docker run -d -p 80:8080 -e PROFILE=production -e GIT_REPO="https://github.com/guedim/spring-projects" \
+    -e LABEL=master -e REL_PATH="externalize-config/src/main/resources" \
+    -e APP_NAME="application" guedim/externalize-config
