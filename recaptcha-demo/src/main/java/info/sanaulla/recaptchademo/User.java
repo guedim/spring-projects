b49eb8f245e3aa0a96a5c7dbb6c4790433ffda8e
Mario Guerrero <guedim@gmail.com>
2017-11-18 18:34:02 -0500

    Update User.java

--- recaptcha-demo/src/main/java/info/sanaulla/recaptchademo/User.java
+++ recaptcha-demo/src/main/java/info/sanaulla/recaptchademo/User.java
@@ -19,6 +19,8 @@ import lombok.Setter;
 @Entity
 @Data
 @NoArgsConstructor
+@Getter
+@Setter
 public class User {
 
 	@Id
@@ -26,7 +28,6 @@ public class User {
 	private Long id;
 	
 	@NotEmpty @Size(max = 50)
-	@Getter @Setter
 	private String firstName;
 	
 	@NotEmpty @Size(max = 50)
@@ -41,57 +42,5 @@ public class User {
 	
 	@Transient
 	@JsonIgnore
-	private String confirmPassword;
-
-  public Long getId() {
-    return id;
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public String getConfirmPassword() {
-    return confirmPassword;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public void setConfirmPassword(String confirmPassword) {
-    this.confirmPassword = confirmPassword;
-  }
-
-	
-	
-	
+	private String confirmPassword;	
 }
