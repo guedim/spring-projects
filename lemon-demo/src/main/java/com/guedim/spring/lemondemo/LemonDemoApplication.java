7d1b6dd0d7fd9a37f64837ed91057931fa4e4b40
Mario Guerrero <guedim@gmail.com>
2018-04-14 22:16:20 -0500

    initial commit

---
+++ lemon-demo/src/main/java/com/guedim/spring/lemondemo/LemonDemoApplication.java
@@ -0,0 +1,12 @@
+package com.guedim.spring.lemondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemonDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemonDemoApplication.class, args);
+	}
+}
