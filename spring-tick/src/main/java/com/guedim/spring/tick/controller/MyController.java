b4677e59968c6dc861490aa185119fb14b18a3c6
Mario Guerrero <guedim@gmail.com>
2018-04-21 15:41:20 -0500

    add spring-boot to docker-compose

---
+++ spring-tick/src/main/java/com/guedim/spring/tick/controller/MyController.java
@@ -0,0 +1,48 @@
+package com.guedim.spring.tick.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@RequestMapping("/")
+    public String index() {
+		delay();
+        return "Greetings from Spring Boot!";
+    }
+
+	@GetMapping("/api/people")
+	public List<Person> listPeople() {
+
+		delay();
+		return getPeople();
+
+	}
+
+	private List<Person> getPeople() {
+		Person p1 = new Person("Mario");
+		Person p2 = new Person("Yuly");
+		Person p3 = new Person("Matías");
+
+		List<Person> people = new ArrayList<>();
+		people.add(p1);
+		people.add(p2);
+		people.add(p3);
+
+		return people;
+	}
+
+	private void delay() {
+		try {
+			Thread.sleep(new Random().nextInt(4) * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ spring-tick/src/main/java/com/guedim/spring/tick/controller/Person.java
@@ -0,0 +1,22 @@
+package com.guedim.spring.tick.controller;
+
+public class Person {
+
+	private String name;
+
+	
+	public Person() {
+	}
+
+	public Person(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}	
+}
