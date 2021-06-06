9dcce40109e0af243b5c3bad9c16960dbc88e304
Mario Guerrero <mario.guerrero@payulatam.com>
2021-06-06 01:01:09 -0500

    project workbench ready

--- CQRS/user-management/user.cmd.api/src/main/java/com/guedim/springbank/user/cmd/api/UserCommandApplication.java
+++ CQRS/user-management/user.cmd.api/src/main/java/com/guedim/springbank/user/cmd/api/UserCommandApplication.java
@@ -1,9 +1,12 @@
 package com.guedim.springbank.user.cmd.api;
 
+import com.guedim.springbank.user.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(AxonConfig.class)
 public class UserCommandApplication {
 
 	public static void main(String[] args) {
