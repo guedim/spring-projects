a71a7ed4eb3ca7ddb9b5eaee04f19f8b46a62dec
Mario Guerrero <mario.guerrero@payulatam.com>
2021-06-06 00:02:16 -0500

    project CQRS structyure

---
+++ CQRS/user-management/user.cmd.api/src/main/java/com/guedim/springbank/user/cmd/api/Application.java
@@ -0,0 +1,13 @@
+package com.guedim.springbank.user.cmd.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
