ffc0e036ce2328a052ae4d40ce4de9bfc183f399
Mario Guerrero <mario.guerrero@payulatam.com>
2021-06-06 23:01:42 -0500

    events user added

---
+++ CQRS/user-management/user.core/src/main/java/com/guedim/springbank/user/core/events/UserRegisteredEvent.java
@@ -0,0 +1,14 @@
+package com.guedim.springbank.user.core.events;
+
+
+import com.guedim.springbank.user.core.models.User;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserRegisteredEvent {
+
+    private String id;
+    private User user;
+}
