b543d90ea27324d7da5d36c0b099fab44af964e4
Mario Guerrero <mario.guerrero@payulatam.com>
2021-06-07 00:16:26 -0500

    register user controller implementtion

--- CQRS/user-management/user.core/src/main/java/com/guedim/springbank/user/core/models/Account.java
+++ CQRS/user-management/user.core/src/main/java/com/guedim/springbank/user/core/models/Account.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -13,7 +15,10 @@ import java.util.List;
 @Builder
 public class Account {
 
+    @Size(min = 2, message = "username must have a minimum of 2 characters")
     private String userName;
+    @Size(min = 7, message = "password must have a minimum of 7 characters")
     private String password;
+    @NotNull(message = "specify at lease 1 user role")
     private List<Role> roles;
 }
--- CQRS/user-management/user.core/src/main/java/com/guedim/springbank/user/core/models/User.java
+++ CQRS/user-management/user.core/src/main/java/com/guedim/springbank/user/core/models/User.java
@@ -7,6 +7,11 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,8 +21,13 @@ public class User {
 
     @Id
     private String id;
+    @NotEmpty(message = "first name is mandatory")
     private String firstName;
+    @NotEmpty(message = "last name is mandatory")
     private String lastname;
+    @Email(message = "please provide a valida email address")
     private String email;
+    @Valid
+    @NotNull(message = "please provide account credentials")
     private Account account;
 }
