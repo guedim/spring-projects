dcf42f13aec6025aaedb35ed49500f75563df42a
Mario Guerrero <mario.guerrero@payulatam.com>
2021-06-07 20:06:54 -0500

    update user command implementation

--- CQRS/user-management/user.cmd.api/src/main/java/com/guedim/springbank/user/cmd/api/commands/UpdateUserCommand.java
+++ CQRS/user-management/user.cmd.api/src/main/java/com/guedim/springbank/user/cmd/api/commands/UpdateUserCommand.java
@@ -5,11 +5,16 @@ import lombok.Builder;
 import lombok.Data;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 @Data
 @Builder
 public class UpdateUserCommand {
 
     @TargetAggregateIdentifier
     private String id;
+    @NotNull(message = "no user details were supplied")
+    @Valid
     private User user;
 }
