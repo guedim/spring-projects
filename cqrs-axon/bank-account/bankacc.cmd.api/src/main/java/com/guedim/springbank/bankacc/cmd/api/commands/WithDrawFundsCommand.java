a5ee6e01effc3deb9d8dda8b96b493cafdd90e32
Mario Guerrero <mario.guerrero@payulatam.com>
2021-07-14 23:08:02 -0500

    delete controller

--- cqrs-axon/bank-account/bankacc.cmd.api/src/main/java/com/guedim/springbank/bankacc/cmd/api/commands/WithDrawFundsCommand.java
+++ cqrs-axon/bank-account/bankacc.cmd.api/src/main/java/com/guedim/springbank/bankacc/cmd/api/commands/WithDrawFundsCommand.java
@@ -4,12 +4,17 @@ import lombok.Builder;
 import lombok.Data;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+
 @Data
 @Builder
+@Valid
 public class WithDrawFundsCommand {
 
     @TargetAggregateIdentifier
     private String id;
+    @Min(value = 1, message = "Withdraw value must be greater than 0")
     private double amount;
 
 }
--- cqrs-axon/bank-account/bankacc.cmd.api/src/main/java/com/guedim/springbank/bankacc/cmd/api/controllers/CloseAccountController.java
+++ cqrs-axon/bank-account/bankacc.cmd.api/src/main/java/com/guedim/springbank/bankacc/cmd/api/controllers/CloseAccountController.java
@@ -1,8 +1,7 @@
 package com.guedim.springbank.bankacc.cmd.api.controllers;
 
-import com.guedim.springbank.bankacc.cmd.api.commands.DepositFundsCommand;
-import com.guedim.springbank.bankacc.cmd.api.commands.OpenAccountCommand;
-import com.guedim.springbank.bankacc.cmd.api.dto.OpenAccountResponse;
+import com.guedim.springbank.bankacc.cmd.api.commands.CloseAccountCommand;
+import com.guedim.springbank.bankacc.cmd.api.commands.WithDrawFundsCommand;
 import com.guedim.springbank.bankacc.core.dto.BaseResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.gateway.CommandGateway;
@@ -13,31 +12,33 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.UUID;
 
 @RestController
-@RequestMapping(path = "/api/v1/depositFunds")
+@RequestMapping(path = "/api/v1/closeBankAccount")
 @Slf4j
-public class DepositFundsController {
+public class CloseAccountController {
 
     private final CommandGateway commandGateway;
 
     @Autowired
-    public DepositFundsController(CommandGateway commandGateway) {
+    public CloseAccountController(CommandGateway commandGateway) {
         this.commandGateway = commandGateway;
     }
 
-    @PutMapping(path = "/{id}")
+    @DeleteMapping(path = "/{id}")
     @PreAuthorize("hasAuthority('WRITE_PRIVILEGE')")
-    public ResponseEntity<BaseResponse> depositFunds(@PathVariable(value = "id") String id,
-                                                     @Valid @RequestBody DepositFundsCommand command) {
+    public ResponseEntity<BaseResponse> closeBankAccount(@PathVariable(value = "id") String id) {
 
         try {
+            var command = CloseAccountCommand.builder()
+                    .id(id)
+                    .build();
+
             command.setId(id);
             commandGateway.send(command);
-            return new ResponseEntity<>(new BaseResponse("Successfully deposit funded !!!"), HttpStatus.OK);
+            return new ResponseEntity<>(new BaseResponse("Successfully close account  completed !!!"), HttpStatus.OK);
         } catch (Exception e) {
-            var safeErrorMessage = "Error while processing request to deposit bank account for id:" + id;
+            var safeErrorMessage = "Error while processing request to close bank account with id:" + id;
             log.error(e.toString());
             return new ResponseEntity<>(new BaseResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
         }
