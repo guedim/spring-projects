eaf02c1b042b2ee5422563d41f1a4b733832616f
Mario Guerrero <mario.guerrero@payulatam.com>
2021-07-14 23:17:06 -0500

    delete controller

---
+++ cqrs-axon/bank-account/bankacc.query.api/src/main/java/com/guedim/springbank/bankacc/query/api/queries/FindAllAccountsQuery.java
@@ -0,0 +1,10 @@
+package com.guedim.springbank.bankacc.query.api.queries;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FindAllAccountsQuery {
+}
